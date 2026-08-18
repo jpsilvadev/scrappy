aac849d96589b03ce829f7414395d74f1958348b
jpsilvadev <jpsjoaosilva98@gmail.com>
2026-08-18 22:19:18 +0100

    allow for max_pages setting

--- crawl.py
+++ crawl.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import TypedDict
+from typing import Self, TypedDict
 from urllib.parse import urljoin, urlsplit
 
 import aiohttp
@@ -20,24 +20,38 @@ class PageData(TypedDict):
 
 
 class AsyncCrawler:
-    def __init__(self, base_url: str) -> None:
+    def __init__(self, base_url: str, max_concurrency: int, max_pages: int) -> None:
         self.base_url = base_url
         self.base_domain = urlsplit(base_url).netloc
         self.page_data: dict[str, PageData] = {}
         self.lock = asyncio.Lock()
-        self.max_concurrency = 3
+        self.max_concurrency = max_concurrency
         self.semaphore = asyncio.Semaphore(self.max_concurrency)
         self.session: aiohttp.ClientSession | None = None
+        self.max_pages = max_pages
+        self.should_stop = False
+        self.all_tasks: set[asyncio.Task] = set()
 
-    async def __aenter__(self):
+    async def __aenter__(self) -> Self:
         self.session = aiohttp.ClientSession()
         return self
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
         if self.session is not None:
             await self.session.close()
 
-    async def add_page_visit(self, normalized_url: str):
+    async def add_page_visit(self, normalized_url: str) -> bool:
+        if self.should_stop:
+            return False
+
+        if len(self.page_data) == self.max_pages:
+            self.should_stop = True
+            print("Reached maximum number of pages to crawl.")
+            for task in self.all_tasks:
+                if not task.done():
+                    task.cancel()
+            return False
+
         async with self.lock:
             # returns true if first time visiting page
             return not normalized_url in self.page_data
@@ -67,6 +81,9 @@ class AsyncCrawler:
         self,
         current_url: str | None = None,
     ) -> None:
+        if self.should_stop:
+            return
+
         if current_url is None:
             current_url = self.base_url
 
@@ -81,6 +98,7 @@ class AsyncCrawler:
 
         async with self.semaphore:
             html = await self.get_html(current_url)
+            print(f"starting crawl of {current_url}")
             if html is None:
                 return
 
@@ -95,17 +113,24 @@ class AsyncCrawler:
         for url in urls:
             task = asyncio.create_task(self.crawl_page(url))
             tasks.append(task)
+            self.all_tasks.add(task)
 
         if tasks:
-            await asyncio.gather(*tasks)
+            try:
+                await asyncio.gather(*tasks, return_exceptions=True)
+            finally:
+                for task in tasks:
+                    self.all_tasks.discard(task)
 
     async def crawl(self) -> dict[str, PageData] | None:
         await self.crawl_page(self.base_url)
         return self.page_data
 
 
-async def crawl_site_async(base_url: str) -> dict[str, PageData] | None:
-    async with AsyncCrawler(base_url) as crawler:
+async def crawl_site_async(
+    base_url: str, max_concurrency: int, max_pages: int
+) -> dict[str, PageData] | None:
+    async with AsyncCrawler(base_url, max_concurrency, max_pages) as crawler:
         return await crawler.crawl()
 
 
--- main.py
+++ main.py
@@ -10,14 +10,20 @@ async def main() -> None:
         print("no website provided")
         sys.exit(1)
 
-    if len(args) > 2:
+    if len(args) > 4:
         print("too many arguments provided")
         sys.exit(1)
 
     base_url = args[1]
-    print(f"starting crawl of: {base_url}")
+    try:
+        max_concurrency = int(args[2])
+        max_pages = int(args[3])
+    except (TypeError, ValueError):
+        print("Incorrect type for either `max_concurrency` or `max_pages`")
+        print("Exiting...")
+        sys.exit(1)
 
-    page_data = await crawl_site_async(base_url)
+    page_data = await crawl_site_async(base_url, max_concurrency, max_pages)
     if page_data is None:
         print("crawl failed")
         sys.exit(1)
